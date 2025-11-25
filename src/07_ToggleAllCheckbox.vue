08586ec0d48e61506dc4ac0c4416f1f9e3af7990
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:09:43 +0530

    Add ToggleAllCheckbox component with toggle functionality

---
+++ src/07_ToggleAllCheckbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <input
+    data-testid="ToggleAllCheckbox"
+    type="checkbox"
+    :checked="allChecked"
+    @change="onToggleAll"
+  />
+</template>
+
+<script setup>
+const props = defineProps({ allChecked: { type: Boolean, default: false } })
+const emit = defineEmits(['toggleAll'])
+function onToggleAll(e) { emit('toggleAll', e.target.checked) }
+</script>
