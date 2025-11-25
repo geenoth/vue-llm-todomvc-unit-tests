fbe7f5c66e4407c19631a307a435f89334417434
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:08:39 +0530

    Add TodoList component with toggle, edit, and destroy functionalities

---
+++ src/05_TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div data-testid="TodoList">
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input
+          data-testid="TodoItemCheckbox"
+          type="checkbox"
+          :checked="todo.completed"
+          @change="() => $emit('toggle', todo.id)"
+        />
+        <label data-testid="TodoLabel" @dblclick="$emit('edit', todo.id)">{{ todo.title }}</label>
+        <button data-testid="DestroyButton" @click="$emit('destroy', todo.id)">×</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  todos: { type: Array, default: () => [] }
+})
+const emit = defineEmits(['toggle', 'destroy', 'edit'])
+</script>
