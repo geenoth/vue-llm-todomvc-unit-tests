8c043e2d3f8b664910f07b0afab6b0ec2a16eae5
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:06:57 +0530

    Add DestroyButton component with emit functionality

---
+++ src/03_DestroyButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <button data-testid="DestroyButton" type="button" @click="onClick" aria-label="Delete">
+    ×
+  </button>
+</template>
+
+<script setup>
+const emit = defineEmits(['destroy'])
+function onClick() { emit('destroy') }
+</script>
