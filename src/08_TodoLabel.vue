df7427ff98528ac1dee76c426f9a1562274e5caf
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:13:17 +0530

    Add TodoLabel component with edit functionality

---
+++ src/08_TodoLabel.vue
@@ -0,0 +1,9 @@
+<template>
+  <span data-testid="TodoLabel" @dblclick="onEdit">{{ title }}</span>
+</template>
+
+<script setup>
+const props = defineProps({ title: { type: String, default: '' } })
+const emit = defineEmits(['edit'])
+function onEdit() { emit('edit') }
+</script>
