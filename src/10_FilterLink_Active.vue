a0a8c2fd53d72f67525f09749e856bc0f5e40b2c
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:15:02 +0530

    Add FilterLink_Active component for active state selection

---
+++ src/10_FilterLink_Active.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    href="#"
+    data-testid="FilterLink_Active"
+    :class="{ selected: active }"
+    @click.prevent="select"
+  >
+    Active
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({ active: { type: Boolean, default: false } })
+const emit = defineEmits(['select'])
+function select() { emit('select', 'active') }
+</script>
