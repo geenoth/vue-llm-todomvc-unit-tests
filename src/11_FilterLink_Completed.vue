2b6c5449fbc7211bfb8e96e495287b6a0ddd3391
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:15:33 +0530

    Add 11_FilterLink_Completed component with template and script setup

---
+++ src/11_FilterLink_Completed.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    href="#"
+    data-testid="FilterLink_Completed"
+    :class="{ selected: active }"
+    @click.prevent="select"
+  >
+    Completed
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({ active: { type: Boolean, default: false } })
+const emit = defineEmits(['select'])
+function select() { emit('select', 'completed') }
+</script>
