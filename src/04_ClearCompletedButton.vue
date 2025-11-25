63774f5d583f830e8447811cad643c6ac4eb6b2a
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:07:52 +0530

    Add Clear Completed Button component with functionality

---
+++ src/04_ClearCompletedButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    data-testid="ClearCompletedButton"
+    type="button"
+    :disabled="!hasCompleted"
+    @click="onClear"
+  >
+    Clear completed
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({ hasCompleted: { type: Boolean, default: false } })
+const emit = defineEmits(['clear'])
+function onClear() { if (props.hasCompleted) emit('clear') }
+</script>
