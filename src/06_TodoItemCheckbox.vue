978a81bbc212a48daf767e7540cbc1bfb34366f6
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:09:15 +0530

    Add TodoItemCheckbox component with checkbox functionality

---
+++ src/06_TodoItemCheckbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <input
+    data-testid="TodoItemCheckbox"
+    type="checkbox"
+    :checked="checked"
+    @change="onChange"
+  />
+</template>
+
+<script setup>
+const props = defineProps({ checked: { type: Boolean, default: false } })
+const emit = defineEmits(['change'])
+function onChange(e) { emit('change', e.target.checked) }
+</script>
