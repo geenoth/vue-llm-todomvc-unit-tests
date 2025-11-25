1b7440f5e456309329c6cbf121baf29c8413a883
Geenoth <gvwgeenoth365@gmail.com>
2025-11-25 12:13:49 +0530

    Add FilterLink_All component for filtering functionality

---
+++ src/09_FilterLink_All.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    href="#"
+    data-testid="FilterLink_All"
+    :class="{ selected: active }"
+    @click.prevent="select"
+  >
+    All
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({ active: { type: Boolean, default: false } })
+const emit = defineEmits(['select'])
+function select() { emit('select', 'all') }
+</script>
